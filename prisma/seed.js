9d4aeed77eb66ed9ee2f1ea2db15623e8946518b
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-05-11 12:43:25 -0700

    inserting values to mission commander table

--- prisma/seed.js
+++ prisma/seed.js
@@ -43,7 +43,56 @@ const prisma = new PrismaClient();
       },
     });
 
+    const missionComander1 = await prisma.missionCommander.upsert({
+        where: { name: 'Mc1' },
+        update: {},
+        create: {
+         name: 'Mc1',
+                 lang: 'JavaScript',
+                 missionCommander: 'Carlo',
+                 enrollments: 400,
+                 hasCertification: true
+        },
+      });
+
+     const missionComander2 = await prisma.missionCommander.upsert({
+        where: { name: 'Mc2' },
+        update: {},
+        create: {
+         name: 'Mc2',
+                 lang: 'Java',
+                 missionCommander: 'Fer',
+                 enrollments: 300,
+                 hasCertification: false
+        },
+      });
+
+      const missionComander3 = await prisma.missionCommander.upsert({
+        where: { name: 'Mc3' },
+        update: {},
+        create: {
+         name: 'Mc3',
+                 lang: 'Java',
+                 missionCommander: 'pepe',
+                 enrollments: 30,
+                 hasCertification: true
+        },
+      });
+
+      const missionComander4 = await prisma.missionCommander.upsert({
+        where: { name: 'Mc4' },
+        update: {},
+        create: {
+         name: 'Mc4',
+                 lang: 'JavaScript',
+                 missionCommander: 'pancho',
+                 enrollments: 100,
+                 hasCertification: false
+        },
+      });
+
     console.log('Create 3 explorers');
+    console.log('Create Mission Commanders')
   } catch(e) {
     console.error(e);
     process.exit(1);
